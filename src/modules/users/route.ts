e56bf98f36d3d36ab0c9e53a6c3710b41a0a9db9
Austin <babelontrip@gmail.com>
2024-12-18 00:26:02 -0600

    Protect crud router with auth middleware

--- src/modules/users/route.ts
+++ src/modules/users/route.ts
@@ -1,8 +1,9 @@
-import {Router} from "express"
+import { Router } from "express"
 import { listUsers, createUser, getUser, deleteUser, updateUser} from "./controller"
+import { auth } from "../../middleware/auth"
 
 export const userRouter = Router()
-
+userRouter.use(auth)
 userRouter.get('', listUsers)
 userRouter.post('',createUser )
 userRouter.get('/', getUser)
