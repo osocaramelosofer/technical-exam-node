ee60bd561b67b9c64736c349317c195eb75bd1bd
Austin <babelontrip@gmail.com>
2024-12-18 08:32:18 -0600

    Add auth validations

--- src/modules/auth/controller.ts
+++ src/modules/auth/controller.ts
@@ -2,13 +2,16 @@ import { Request, Response } from 'express'
 import { authService } from './service'
 
 
-export async function login(req: Request, res: Response): Promise<any> {
+export async function login(req: Request, res: Response) {
   try {
     const { email, password } = req.body;
 
-    const foundUser = await authService.login({email, password})
-    res.status(200).send(foundUser); 
+    const token = await authService.login({email, password})
+    if(!token){
+      res.status(500).json({ error: "Failed to generate token" })
+    }
 
+      res.status(200).send({token}); 
   } catch (error) {
     console.error("Error during login:", error)
     res.status(500).json({ error: "Internal server error" })
--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -1,11 +1,11 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
 import User from '../../models/User'
-import { generateToken } from '../../lib/helpers';
+import { generateToken } from '../../lib/helpers'
 
 
 
 export const authService = {
-  login: async({email, password} : { email: string, password: string}):Promise<any> => {
+  login: async({email, password} : { email: string, password: string}):Promise<string | null> => {
     try {
       const foundUser = await User.findOne({ email })
 
@@ -16,18 +16,16 @@ export const authService = {
       const isMatch = bcrypt.compareSync(password, foundUser.password);
   
       if (isMatch) {
-        // HERE replace the jwt.sign with a method
-        // const token = jwt.sign({ _id: foundUser._id?.toString(), name: foundUser.name }, SECRET_KEY, {
-        //   expiresIn: '2 days',
-        // });
         const token = generateToken(foundUser)
    
-        return { token: token };
+        return token
       } else {
-        throw new Error('Password is not correct');
+        throw new Error('Password is not correct')
       }
     } catch (error) {
-      
+      const message = error instanceof Error ? error.message : error
+      console.error("Something went wrong generating the token:", message)
+      return null
     }
   }
 }
