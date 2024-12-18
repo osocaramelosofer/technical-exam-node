1b9ac8aa8032579df917338b9c7858fb2e190d62
Austin <babelontrip@gmail.com>
2024-12-18 00:27:47 -0600

    Add auth router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { createApp } from './config/express';
 import { connectToDatabase } from './config/mongodb';
 import { userRouter } from './modules/users/route';
+import authRouter from './modules/auth/route';
 
 const app = createApp();
 const port = process.env.PORT || 3000;
@@ -11,9 +12,12 @@ const startServer = async () => {
     await connectToDatabase()
 
     app.get('/', (req: Request, res: Response) => {
-      res.send('Hello, TypeScript with Express!!');
+      res.send('Hello!');
     });
-    app.use('/api/v1/users',userRouter)
+
+
+    app.use('/api/v1/users', userRouter)
+    app.use('/api/v1/auth', authRouter)
     
     app.listen(port, () => {
       console.log(`Server is running at http://localhost:${port}`);
